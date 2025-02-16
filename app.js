c79acb6ecdaa7b50976b67cf98b01b4592ce80c7
0x3C-0x3E <rbng0094@gmail.com>
2025-02-16 19:43:01 +0100

    Add files via upload

---
+++ app.js
@@ -0,0 +1,28 @@
+// Registering Service Worker
+if ('serviceWorker' in navigator) {
+  navigator.serviceWorker.register('/pwa-examples/js13kpwa/sw.js');
+}
+
+// Requesting permission for Notifications after clicking on the button
+const button = document.getElementById('notifications');
+button.addEventListener('click', () => {
+  Notification.requestPermission().then((result) => {
+    if (result === 'granted') {
+      randomNotification();
+    }
+  });
+});
+
+// Setting up random Notification
+function randomNotification() {
+  const randomItem = Math.floor(Math.random() * games.length);
+  const notifTitle = games[randomItem].name;
+  const notifBody = `Created by ${games[randomItem].author}.`;
+  const notifImg = `data/img/${games[randomItem].slug}.jpg`;
+  const options = {
+    body: notifBody,
+    icon: notifImg,
+  };
+  new Notification(notifTitle, options);
+  setTimeout(randomNotification, 30000);
+}
